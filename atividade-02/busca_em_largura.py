358776bee05dfbafd68206c4bd7532aa3f3e257e
João Victor Carvalho <joaocarvoli@hotmail.com>
2021-11-22 20:05:14 -0300

    Update busca_em_largura.py

--- atividade-02/busca_em_largura.py
+++ atividade-02/busca_em_largura.py
@@ -46,6 +46,8 @@ class Graph:
             if len(g.graph[toFound]) == 0:
                 return print(fail)
             self.graph[toFound]
+            #...
+            
 
 g = Graph() 
 
@@ -110,20 +112,3 @@ print(g.graph['Zerind'])
 g.breadthSearch('Fortaleza')"""
 
 print(g.graph)
-
-
-def bfs(visit_complete, graph, current_node):
-    visit_complete.append(current_node)
-    queue = []
-    queue.append(current_node)
- 
-    while queue:
-        s = queue.pop(0)
-        print(s)
- 
-        for neighbour in graph[s]:
-            if neighbour not in visit_complete:
-                visit_complete.append(neighbour)
-                queue.append(neighbour)
- 
-bfs([], g, 'Zerind')
