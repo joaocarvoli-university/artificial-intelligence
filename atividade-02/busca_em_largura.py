ee45d3937b2ca8283f76631c6f1c872ba9cc3f5f
joaocarvoli <joaocarvoli@hotmail.com>
2021-11-23 22:15:00 -0300

    busca em largura

--- atividade-02/busca_em_largura.py
+++ atividade-02/busca_em_largura.py
@@ -38,25 +38,70 @@ class Graph:
             for edges in self.graph[vertex]:
                 print(f'{vertex} -> {edges[0]} peso: {edges[1]}')
 
-    def breadthSearch(self, toFound):
+    # Algoritmo de busca em largura
+    def breadthSearch(self ,fromW, toFound):
         fail = 'falha, o nó não possui relacionamentos!'
+        fromWhere = self.graph[fromW] # essa variável carrega os filhos de fromW
         explored = []
 
         while True:
-            if len(g.graph[toFound]) == 0:
+            if len(g.graph[fromW]) == 0: # Verifica se o nó a ser encontrado não tem nenhum relacionamento
                 return print(fail)
-            self.graph[toFound]
+            print(fromWhere)
+            #visited = fromWhere.pop() # Remove apenas o primeiro elemento do relacionamento do nó
+            #explored.append(visited)   # Adiciona o elemento removido a lista de explorados
+
+            for children in fromWhere: # Iterando sobre os nós filhos restantes
+                #criar um filho (variável x) na árvore de busca?
+                print(self.graph[children[0]])
+                if children not in explored or fromWhere:
+                    if children == toFound:
+                        return print("Filho encontrado!")
+            
+            
+            return 0 # para não entrar em loop infinito nos testes
             #...
             
+    def buscaEmLargura(self ,fromWhere, toFound):
+        fail = 'falha, o nó não possui relacionamentos!'
+        explored = []
+        edge = []
+
+        edge.append(fromWhere)
+        explored.append(fromWhere)
+        edge.pop()
+
+        for children in self.graph[fromWhere]:
+            edge.append(children[0])
+            print(f'Borda {edge}')
+            for subChildren in edge:
+                print(self.graph[subChildren])
+
+    def bfs(self, node):
+        visited = [] # List to keep track of visited nodes.
+        queue = []     #Initialize a queue 
+        visited.append(node)
+        queue.append(node)
+
+        while queue:
+            s = queue.pop(0) 
+            print (s, end = "->")
+
+            for neighbour in self.graph[s]:
+                neighbour = neighbour[0]
+                if neighbour not in visited:
+                    visited.append(neighbour)
+                    queue.append(neighbour)
+
 
 g = Graph() 
 
 # Armazenamento do número de vértices no Grafo
 vertices_no = 0
+g.add_vertex('Arad')
 g.add_vertex('Zerind')
 g.add_vertex('Oradea')
 g.add_vertex('Sibiu')
-g.add_vertex('Arad')
 g.add_vertex('Timisoara')
 g.add_vertex('Lugoj')
 g.add_vertex('Mehadia')
@@ -73,13 +118,13 @@ g.add_vertex('Eforie')
 g.add_vertex('Vaslui')
 g.add_vertex('Iasi')
 g.add_vertex('Neamt')
-g.add_vertex('Petesti')
-g.add_vertex('Fortaleza')
 
 # Adicionando as arestas entre os vértices
 # Structure: from / to / weight
 
 g.add_edge('Oradea', 'Zerind', 71)
+g.add_edge('Sibiu', 'Oradea', 151)
+
 g.add_edge('Zerind', 'Arad', 75)
 g.add_edge('Arad', 'Timisoara', 118)
 g.add_edge('Arad', 'Sibiu', 140)
@@ -88,11 +133,10 @@ g.add_edge('Lugoj', 'Mehadia', 70)
 g.add_edge('Mehadia', 'Drobeta', 75)
 g.add_edge('Drobeta', 'Craiova', 120)
 g.add_edge('Craiova', 'Rimnicu Vilcea', 146)
-g.add_edge('Craiova', 'Petesti', 138)
+g.add_edge('Craiova', 'Pitesti', 138)
 g.add_edge('Pitesti', 'Rimnicu Vilcea', 97)
 g.add_edge('Pitesti', 'Bucharest', 97)
 g.add_edge('Rimnicu Vilcea', 'Sibiu', 80)
-g.add_edge('Sibiu', 'Oradea', 151)
 g.add_edge('Sibiu', 'Fagaras', 99)
 g.add_edge('Fagaras', 'Bucharest', 211)
 g.add_edge('Bucharest', 'Giurgiu', 90)
@@ -103,12 +147,4 @@ g.add_edge('Urziceni', 'Vaslui', 142)
 g.add_edge('Vaslui', 'Iasi', 92)
 g.add_edge('Iasi', 'Neamt', 87)
 
-#g.print_graph()
-
-#g.BFS('Sibiu')
-"""print(g.graph['Zerind'])
-print(g.graph['Zerind'].pop(0))
-print(g.graph['Zerind'])
-g.breadthSearch('Fortaleza')"""
-
-print(g.graph)
+g.bfs('Arad')
