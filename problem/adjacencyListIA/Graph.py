9f1c64c1f1e0a318cce0c5963948e9c6e351f837
joaocarvoli <joaocarvoli@hotmail.com>
2021-11-27 00:02:45 -0300

    organizing all

---
+++ problem/adjacencyListIA/Graph.py
@@ -0,0 +1,24 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_vertex(self, v):
+        global vertices_no
+
+        if v in self.graph:
+            print(f"The vertex {v} already was created!")
+        else:
+            vertices_no = vertices_no + 1
+            self.graph[v] = []
+
+    def add_edge(self, v1, v2, weight):
+        if v1 not in self.graph:
+            print(f'There is no {v1} vertex!')
+        elif v2 not in self.graph:
+            print(f'There is no {v2} vertex!')
+        else:
+            temp = [v2, weight]
+            temp2 = [v1, weight]
+            self.graph[v1].append(temp)
+            self.graph[v2].append(temp2)
+
