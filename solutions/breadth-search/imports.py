102b59d40bc67fef3d8b4dc9ab4bc12efcd77983
joaocarvoli <joaocarvoli@hotmail.com>
2021-12-08 09:25:45 -0300

    renaming folder

--- solutions/breadth-search/imports.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(1, "../../problem/adjacencyListIA/Graph.py")
-sys.path.insert(1, "../../problem/adjacencyListIA/Built.py")
--- solutions/depthFirst-search/search.py
+++ solutions/depthFirst-search/search.py
@@ -4,17 +4,17 @@ from Built import createVertex, createEdges
 
 # Deep Search 
 def deep_search(self, origem, destino):
-    stack = [[origem]] 
-    visited = [] 
+    stack = [[origem]]
+    visited = []
 
-    while (len(stack) != 0):
-        path = stack.pop(0) 
+    while len(stack) != 0:
+        path = stack.pop(0)
         node = path[-1]
 
         if node == destino:
             return print("caminho igual a: ", path)
 
-        for child , _ in self.graph[node]:
+        for child, _ in self.graph[node]:
             if child not in visited:
                 new_path = path + [child]
                 stack.insert(0, new_path)
