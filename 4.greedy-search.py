1cc769655b017adb28c0cb9bbe502e44a14cc5e1
joaocarvoli <joaocarvoli@hotmail.com>
2021-12-08 21:12:12 -0300

    greedy search: test version

--- 4.greedy-search.py
+++ 4.greedy-search.py
@@ -13,4 +13,46 @@ def getHeuristic(city):
     return heuristic[city]
 
 
+def getCost(node):
+    return node[1]
+
+
+def greedy(self, fromWh, toFound):
+    fail = "fail, this node doesn't have any relationship!"
+    explored = [fromWh]
+    visited = []
+    queue = [fromWh]
+    index, smallestCost, costLastNode = [0, 0, 0]
+
+    while len(queue) != 0:
+        city = queue.pop(0)
+        if len(self.graph[city]) == 0:
+            return print(fail)
+        if city == toFound:
+            return print(f'The smallest cost from {fromWh} to {smallestCost[0]} is {smallestCost[1]}')
+
+        for neighbour in self.graph[city]:
+            if (index > 0) and (neighbour[0] not in explored):
+                neighbour[1] = getHeuristic(neighbour[0]) + costLastNode
+                visited.append(neighbour)
+            elif index == 0:
+                neighbour[1] = getHeuristic(neighbour[0])
+                visited.append(neighbour)
+
+        for node in visited:
+            smallestCost = sorted(visited, key=getCost)[0]
+        visited.pop(visited.index(smallestCost))
+        explored.append(smallestCost[0])
+        queue.append(smallestCost[0])
+
+        costLastNode = smallestCost[1]
+
+        index = index + 1
+
+
+graph = Graph()
+createVertex(graph)
+createEdges(graph)
+
+greedy(graph, 'Arad', 'Bucharest')
 
